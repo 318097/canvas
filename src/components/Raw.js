417c889be2a2b47084e3dfa8c21f521cbb7cf55e
Mehul Lakhanpal <318097@gmail.com>
2025-04-01 12:25:02 +0530

    Refactor

--- src/components/Raw.js
+++ src/components/Raw.js
@@ -11,205 +11,10 @@ import * as htmlToImage from "html-to-image";
 import download from "downloadjs";
 import "./Raw.scss";
 import cn from "classnames";
+import { Input, Select, Button } from "antd";
+import { getDefaultContent, PROPERTIES, CONFIG } from "./config";
 
-const PROPERTIES = [
-  {
-    label: "Font Size",
-    key: "text-size",
-    options: [
-      {
-        label: "Base",
-        value: "text-base",
-      },
-      {
-        label: "XS",
-        value: "text-xs",
-      },
-      {
-        label: "SM",
-        value: "text-sm",
-      },
-      {
-        label: "MD",
-        value: "text-md",
-      },
-      {
-        label: "LG",
-        value: "text-3xl",
-      },
-      {
-        label: "XL",
-        value: "text-4xl",
-      },
-      {
-        label: "2XL",
-        value: "text-5xl",
-      },
-      {
-        label: "3XL",
-        value: "text-6xl",
-      },
-      {
-        label: "4XL",
-        value: "text-7xl",
-      },
-    ],
-  },
-  {
-    label: "Text Align",
-    key: "text-align",
-    options: [
-      {
-        label: "Left",
-        value: "text-left",
-      },
-      {
-        label: "Center",
-        value: "text-center",
-      },
-      {
-        label: "Right",
-        value: "text-right",
-      },
-      {
-        label: "Justify",
-        value: "text-justify",
-      },
-    ],
-  },
-  {
-    label: "Text Color",
-    key: "text-color",
-    options: [
-      {
-        label: "Red",
-        value: "text-red-500",
-      },
-      {
-        label: "Blue",
-        value: "text-blue-500",
-      },
-      {
-        label: "Green",
-        value: "text-green-500",
-      },
-      {
-        label: "Yellow",
-        value: "text-yellow-500",
-      },
-      {
-        label: "Purple",
-        value: "text-purple-500",
-      },
-      {
-        label: "Pink",
-        value: "text-pink-500",
-      },
-      {
-        label: "Orange",
-        value: "text-orange-500",
-      },
-      {
-        label: "Teal",
-        value: "text-teal-500",
-      },
-      {
-        label: "Cyan",
-        value: "text-cyan-500",
-      },
-      {
-        label: "Lime",
-        value: "text-lime-500",
-      },
-    ],
-  },
-  {
-    label: "Font Weight",
-    key: "text-weight",
-    options: [
-      {
-        label: "Thin",
-        value: "font-thin",
-      },
-      {
-        label: "Extra Light",
-        value: "font-extralight",
-      },
-      {
-        label: "Light",
-        value: "font-light",
-      },
-      {
-        label: "Normal",
-        value: "font-normal",
-      },
-      {
-        label: "Medium",
-        value: "font-medium",
-      },
-      {
-        label: "Semi Bold",
-        value: "font-semibold",
-      },
-      {
-        label: "Bold",
-        value: "font-bold",
-      },
-      {
-        label: "Extra Bold",
-        value: "font-extrabold",
-      },
-      {
-        label: "Black",
-        value: "font-black",
-      },
-    ],
-  },
-];
-
-const CONFIG = [
-  {
-    platform: "INSTAGRAM",
-    fontMultiplier: 2,
-    className: `h-[1080px] w-[1080px]`,
-    layout: [
-      {
-        type: "title",
-        key: "title",
-        className: "leading-relaxed",
-        properties: {
-          "text-size": "text-5xl",
-          "text-weight": "font-bold",
-          "text-align": "text-center",
-          "text-color": "text-white",
-        },
-      },
-      {
-        type: "paragraph",
-        key: "content",
-        className: "leading-relaxed",
-        properties: {
-          "text-size": "text-3xl",
-          "text-align": "text-left",
-          "text-color": "text-white",
-        },
-      },
-    ],
-  },
-];
-
-const getDefaultContent = () => {
-  return {
-    title: "Custom `.pick` for objects",
-    content: `
-- snake_case  
-- keb-case  
-- camelCase (JS variables, functions)  
-- PascalCase (JS Classes)  
-- SCREAMING_SNAKE_CASE (JS constants)  
-`,
-  };
-};
+const { TextArea } = Input;
 
 const Raw = () => {
   const [data, setData] = useState(getDefaultContent());
@@ -227,6 +32,7 @@ const Raw = () => {
   }, [selected]);
 
   const handleDownload = useCallback(() => {
+    setSelected("");
     if (ref.current === null) {
       return;
     }
@@ -249,30 +55,29 @@ const Raw = () => {
       });
   }, [ref]);
 
-  const handleChange = (e, key) => {
+  const handleChange = (key, value) => {
     setProperties((prev) => ({
       ...prev,
-      classes: cn(prev.classes, e.target.value),
+      [key]: value,
     }));
+
     const [, section] = selected.split(":");
+
     setConfig((prev) => {
       const newConfig = [...prev];
       const match = newConfig[0].layout.find((item) => item.key === section);
-      match.properties[key] = e.target.value;
+      match.properties[key] = value;
       return newConfig;
     });
   };
 
   return (
-    <div className="flex items-center flex-col p-1">
-      <header class="flex items-center justify-between w-2/3 p-4 bg-green-400 rounded-xl">
+    <div className="flex items-center flex-col p-0">
+      <header class="flex items-center justify-between w-full p-4 bg-green-100">
         <h3 className="text-white font-bold">Canvas</h3>
-        <button className="text-white text-sm" onClick={handleDownload}>
-          Download
-        </button>
       </header>
-      <div className="flex items-start gap-2">
-        <div className="p-1 rounded-xl bg-gray-400">
+      <div className="flex items-start gap-0 w-full">
+        <div className="p-2 bg-white grow">
           {config.map((config) => {
             const { layout, className = "", fontMultiplier, platform } = config;
             return (
@@ -306,22 +111,17 @@ const Raw = () => {
                       }
                     );
                     return (
-                      <div className="atom">
-                        <div
-                          onClick={() => {
-                            setSelected(fullKey);
-                          }}
-                          className={classNames}
-                          dangerouslySetInnerHTML={{
-                            __html: marked
-                              .parseInline(value)
-                              .replace(/^<p>|<\/p>$/g, ""),
-                          }}
-                        ></div>
-                        <div className="actions">
-                          <div class="action">align</div>
-                        </div>
-                      </div>
+                      <div
+                        onClick={() => {
+                          setSelected(fullKey);
+                        }}
+                        className={classNames}
+                        dangerouslySetInnerHTML={{
+                          __html: marked
+                            .parseInline(value)
+                            .replace(/^<p>|<\/p>$/g, ""),
+                        }}
+                      ></div>
                     );
                   })}
                 </div>
@@ -329,21 +129,23 @@ const Raw = () => {
             );
           })}
         </div>
-        <div className="flex flex-col gap-2 rounded-xl bg-gray-400 p-2 w-1/3">
-          <div className="flex flex-col gap-2">
-            <label className="text-sm font-bold">Title</label>
-            <input
-              type="text"
-              className="p-2 rounded-xl bg-gray-800 text-white"
+        <div className="flex flex-col gap-2 bg-gray-100 border border-l-gray-300 p-2 basis-1/6">
+          <div className="flex flex-col items-start gap-1 mb-2">
+            <label className="text-xs font-bold">Title</label>
+            <TextArea
+              rows={4}
+              placeholder="Title"
+              maxLength={6}
               value={data.title}
               onChange={(e) => setData({ ...data, title: e.target.value })}
             />
           </div>
-          <div className="flex flex-col gap-2">
-            <label className="text-sm font-bold">Content</label>
-            <textarea
-              rows={10}
-              className="p-2 rounded-xl bg-gray-800 text-white"
+          <div className="flex flex-col items-start gap-1 mb-2">
+            <label className="text-xs font-bold">Content</label>
+            <TextArea
+              rows={4}
+              placeholder="Content"
+              maxLength={6}
               value={data.content}
               onChange={(e) => setData({ ...data, content: e.target.value })}
             />
@@ -351,37 +153,29 @@ const Raw = () => {
           {!!selected && (
             <Fragment>
               <hr />
-              <h3 className="text-sm font-bold">Properties: {selected}</h3>
+              <h3 className="text-sm font-bold text-left">
+                Properties: {selected}
+              </h3>
               {PROPERTIES.map((property) => {
                 const { options, label, key } = property;
 
                 const value = _.get(properties, key, "");
                 return (
-                  <div className="flex flex-col gap-2">
-                    <label className="text-sm font-bold">{label}</label>
-                    <select
-                      className="p-2 rounded-xl bg-gray-800 text-white"
-                      onChange={(e) => handleChange(e, key)}
+                  <div className="flex flex-col items-start gap-1 mb-2">
+                    <label className="text-xs font-bold">{label}</label>
+                    <Select
+                      options={options}
+                      onChange={(value) => handleChange(key, value)}
                       value={value}
-                    >
-                      {options.map((option) => {
-                        const { label, value } = option;
-                        return (
-                          <option
-                            key={value}
-                            value={value}
-                            className="p-2 rounded-xl bg-gray-800 text-white"
-                          >
-                            {label}
-                          </option>
-                        );
-                      })}
-                    </select>
+                      className="w-full"
+                    />
                   </div>
                 );
               })}
             </Fragment>
           )}
+          <hr />
+          <Button onClick={handleDownload}>Download</Button>
         </div>
       </div>
     </div>
---
+++ src/components/config.js
@@ -0,0 +1,240 @@
+const PROPERTIES = [
+  {
+    label: "Font Size",
+    key: "text-size",
+    options: [
+      {
+        label: "Base",
+        value: "text-base",
+      },
+      {
+        label: "XS",
+        value: "text-xs",
+      },
+      {
+        label: "SM",
+        value: "text-sm",
+      },
+      {
+        label: "MD",
+        value: "text-md",
+      },
+      {
+        label: "LG",
+        value: "text-3xl",
+      },
+      {
+        label: "XL",
+        value: "text-4xl",
+      },
+      {
+        label: "2XL",
+        value: "text-5xl",
+      },
+      {
+        label: "3XL",
+        value: "text-6xl",
+      },
+      {
+        label: "4XL",
+        value: "text-7xl",
+      },
+    ],
+  },
+  {
+    label: "Text Align",
+    key: "text-align",
+    options: [
+      {
+        label: "Left",
+        value: "text-left",
+      },
+      {
+        label: "Center",
+        value: "text-center",
+      },
+      {
+        label: "Right",
+        value: "text-right",
+      },
+      {
+        label: "Justify",
+        value: "text-justify",
+      },
+    ],
+  },
+  {
+    label: "Text Color",
+    key: "text-color",
+    options: [
+      {
+        label: "White",
+        value: "text-white",
+      },
+      {
+        label: "Gray-100",
+        value: "text-gray-100",
+      },
+      {
+        label: "Gray-200",
+        value: "text-gray-200",
+      },
+      {
+        label: "Gray-300",
+        value: "text-gray-300",
+      },
+      {
+        label: "Gray-400",
+        value: "text-gray-400",
+      },
+      {
+        label: "Gray-500",
+        value: "text-gray-500",
+      },
+      {
+        label: "Gray-600",
+        value: "text-gray-600",
+      },
+      {
+        label: "Gray-700",
+        value: "text-gray-700",
+      },
+      {
+        label: "Gray-800",
+        value: "text-gray-800",
+      },
+      {
+        label: "Gray-900",
+        value: "text-gray-900",
+      },
+      {
+        label: "Red",
+        value: "text-red-500",
+      },
+      {
+        label: "Blue",
+        value: "text-blue-500",
+      },
+      {
+        label: "Green",
+        value: "text-green-500",
+      },
+      {
+        label: "Yellow",
+        value: "text-yellow-500",
+      },
+      {
+        label: "Purple",
+        value: "text-purple-500",
+      },
+      {
+        label: "Pink",
+        value: "text-pink-500",
+      },
+      {
+        label: "Orange",
+        value: "text-orange-500",
+      },
+      {
+        label: "Teal",
+        value: "text-teal-500",
+      },
+      {
+        label: "Cyan",
+        value: "text-cyan-500",
+      },
+      {
+        label: "Lime",
+        value: "text-lime-500",
+      },
+    ],
+  },
+  {
+    label: "Font Weight",
+    key: "text-weight",
+    options: [
+      {
+        label: "Thin",
+        value: "font-thin",
+      },
+      {
+        label: "Extra Light",
+        value: "font-extralight",
+      },
+      {
+        label: "Light",
+        value: "font-light",
+      },
+      {
+        label: "Normal",
+        value: "font-normal",
+      },
+      {
+        label: "Medium",
+        value: "font-medium",
+      },
+      {
+        label: "Semi Bold",
+        value: "font-semibold",
+      },
+      {
+        label: "Bold",
+        value: "font-bold",
+      },
+      {
+        label: "Extra Bold",
+        value: "font-extrabold",
+      },
+      {
+        label: "Black",
+        value: "font-black",
+      },
+    ],
+  },
+];
+
+const CONFIG = [
+  {
+    platform: "INSTAGRAM",
+    fontMultiplier: 2,
+    className: `h-[1080px] w-[1080px]`,
+    layout: [
+      {
+        type: "title",
+        key: "title",
+        className: "leading-relaxed",
+        properties: {
+          "text-size": "text-5xl",
+          "text-weight": "font-bold",
+          "text-align": "text-center",
+          "text-color": "text-white",
+        },
+      },
+      {
+        type: "paragraph",
+        key: "content",
+        className: "leading-relaxed",
+        properties: {
+          "text-size": "text-3xl",
+          "text-align": "text-left",
+          "text-color": "text-white",
+        },
+      },
+    ],
+  },
+];
+
+const getDefaultContent = () => {
+  return {
+    title: "Custom `.pick` for objects",
+    content: `
+- snake_case  
+- keb-case  
+- camelCase (JS variables, functions)  
+- PascalCase (JS Classes)  
+- SCREAMING_SNAKE_CASE (JS constants)  
+`,
+  };
+};
+
+export { getDefaultContent, PROPERTIES, CONFIG };
